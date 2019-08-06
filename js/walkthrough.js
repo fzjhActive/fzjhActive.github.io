2fd3b116ed0c69afc9a92fd8a17f586d90b39380
wqhot <wqhot@outlook.com>
2019-08-06 12:52:32 +0800

    walkthrough

---
+++ js/walkthrough.js
@@ -0,0 +1,156 @@
+var arr = [
+        {
+            name: "武学调整介绍", 
+            children: [
+                {
+                    name: "武学机制攻略",
+                    children: [
+                        {name: "放置江湖2019.5.31公式变化攻略v1.2.pdf", },
+                        {name: "放置江湖2019.3.20武学机制更新攻略v1.1.pdf",},
+                        {name: "放置江湖2019.1.28武学与机制更新攻略v1.2.pdf"},
+                        {name: "萌新攻略之——你的平A输出和什么有关(矫正version1.1).pdf"},
+						{name: "装备防护位置.png"},
+						
+                    ]
+                },
+                {
+                    name: "门派、江湖武学调整",
+                    children: [
+                        {name: "武学解锁等级2019.7.9.pdf"},
+                        {name: "周年庆武学调整攻略v1.1.pdf"},
+                        {name: "放置江湖2018.10.25武学更新攻略v1.1.pdf"},
+                        
+                    ]
+                },
+                {
+                    name: "知识类武学",
+                    children: [
+                        {name: "振槁玄经注解.pdf"},
+                        {name: "新版长生诀注解.pdf"},
+                       
+                    ]
+                }
+            ]
+        },
+        {
+            name: "经脉、神兵",
+            children: [
+                {
+                    name: "经脉", 
+                    children: [
+                        {name: "战斗经脉天赋详解v1.1.pdf"},
+                        {name: "经脉天赋个人理解.pdf"},
+                        {name: "经脉固本加点个人理解.pdf"},
+						{name: "经脉固本加点个人理解-抄作业版v1.1.xlsx"},
+                    ]
+                },
+                {
+                    name: "神兵",
+                    children: [
+                        {name: "神兵特性浅析.pdf"},
+                        {name: "武器外观.png"},
+                        {name: "武学对应武器类型.xlsx"},
+
+                    ]
+                },
+                
+            ]
+        },
+        {
+            name: "家园攻略",
+            children: [
+                {
+                    name: "家园系统v1.3.13", 
+                    children: [
+                        {name: "0.1 前言（请先读我！）.pdf"},
+                        {name: "0.2 更新记录.txt"},
+                        {name: "1.1 购买房契与地契说明.pdf"},
+                        {name: "1.2 房型平面图.png"},
+						{name: "1.3 房型列表.xlsx"},
+						{name: "2.1 招募与忠诚度.pdf"},
+						{name: "2.2 管家仆人与门客特性表.xlsx"},
+						{name: "2.3 人物模板.xlsx"},
+						{name: "3.1 特殊房间使用介绍-仆人与家具篇.pdf"},
+						{name: "3.2 特殊房间使用介绍-门客篇.pdf"},
+						{name: "3.3 家具大全.xlsx"},
+						{name: "4.1 房间改造说明.pdf"},
+						{name: "4.2 房屋扩建说明.pdf"},
+						{name: "4.3 普通房间改造价格表.xlsx"},
+						{name: "5.1 身世任务.pdf"},
+						{name: "5.2 身世描述.xlsx"},
+                    ]
+                },
+                {
+                    name: "怪客与拜访",
+                    children: [
+                        {name: "新拜访任务攻略.pdf"},
+                        {name: "江湖怪客玩法攻略.xlsx"},
+
+                    ]
+                },
+                
+            ]
+        },
+		{
+            name: "日常活动",
+            children: [
+                {
+                    name: "师门任务", 
+                    children: [
+                        {name: "师门任务攻略v0.4.pdf"},
+                        {name: "声望头衔v1.2.pdf"},
+
+                    ]
+                },
+                {
+                    name: "挂机任务",
+                    children: [
+                        {name: "挂机与历练收益计算器v2.6.xlsx"},
+                        {name: "挂机与历练收益更新解析v1.1.pdf"},
+                        {name: "挂机与历练收益更新解析v1.1.pdf"},
+  
+                    ]
+                },
+                {
+                    name: "挖宝与挑战",
+                    children: [
+                        {name: "挖宝攻略补充v1.2.pdf"},
+                        {name: "挑战任务攻略补充v1.1.pdf"},
+
+                    ]
+                },
+				{
+                    name: "书页与残页",
+                    children: [
+                        {name: "书页武学与残页获取指南v181228.xlsx"},
+                        {name: "挑战任务攻略补充v1.1.pdf"},
+                        
+                    ]
+                },
+				{
+                    name: "其他杂项",
+                    children: [
+                        {name: "放置江湖正负神攻略.pdf"},
+                        {name: "官员政绩与称号.png"},
+                        {name: "殿试辅助工具使用说明.pdf"},
+                        {name: "乡试工具使用说明.pdf"},
+						{name: "神书任务攻略V1.1.pdf"},
+                    ]
+                },
+            ]
+        },
+		{
+            name: "节日活动攻略",
+            children: [
+                {name: "放置江湖2019七夕活动攻略.pdf"},
+				{name: "无间林任务初版攻略.pdf"},
+				{name: "放置江湖2019春分活动.pdf"},
+				{name: "放置江湖2019元宵活动攻略.pdf"},
+				{name: "放置江湖2019春节活动攻略v1.3.pdf"},
+				{name: "放置江湖2018中秋活动攻略v1.2.pdf"},
+				{name: "七夕配对.pdf"},
+				{name: "2018放置江湖端午龙舟赛v1.1.pdf"},
+                
+            ]
+        },
+    ];
